c9a7a979db088bad679124a32dc534112ef50fcc
matiasceb <matiasceb8812@gmail.com>
2021-10-05 20:33:13 -0300

    Hello Sagas

--- main.js
+++ main.js
@@ -2,14 +2,24 @@ import "babel-polyfill"
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore, applyMiddleware } from 'redux'
 
 import Counter from './Counter'
 import reducer from './reducers'
 
-const store = createStore(reducer)
+import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
+
+import { helloSaga } from './sagas'
+
+const sagaMiddleware = createSagaMiddleware()
+const store = createStore(
+  reducer,
+  applyMiddleware(sagaMiddleware)
+)
+sagaMiddleware.run(helloSaga)
 
 const action = type => store.dispatch({type})
+
 
 function render() {
   ReactDOM.render(
